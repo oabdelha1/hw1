62c37fdc79ec5c3e423b884b1d5d049c1440f5f3
Omar Abd-Elhamid <oabdelha@usc.edu>
2023-01-30 18:23:49 -0800

    finished pt 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -6,8 +6,54 @@
 #include "ulliststr.h"
 
 //Use this file to test your ulliststr implementation before running the test suite
+using namespace std;
+
 
 int main(int argc, char* argv[])
 {
+    ULListStr dat;
 
+
+    cout << 1 << endl;
+    dat.push_back("1");
+    cout << 2 << endl;
+    dat.push_back("123");
+    cout << dat.get(0) << dat.get(1) <<dat.get(2) << endl;
+    dat.push_back("0123456");
+    dat.push_back("abc");
+    //cout << dat.get(3) <<endl;
+    //cout << dat.get(4) <<endl;
+    //cout << dat.get(5) <<endl;
+    //cout << dat.get(10) << dat.get(11) << dat.get(12)<< endl;
+    cout << dat.get(0) << dat.get(1) <<dat.get(2) <<dat.get(3) <<dat.get(4)<<dat.get(5);
+    cout <<dat.get(6)<<dat.get(7)<<dat.get(8)<<dat.get(9)<<dat.get(10)<<dat.get(11)<<dat.get(12);
+    cout <<dat.get(13)<<dat.get(14)<<endl;
+    cout << dat.size() << endl;
+
+    size_t test = 0;
+    cout << test << endl;
+    test--;
+    cout << test << endl;
+
+    ULListStr fat;
+
+    cout << "A" << endl;
+    fat.push_front("1");
+    fat.push_front("abc");
+
+    cout << fat.get(0) << fat.get(1) << fat.get(2) << fat.get(3) << endl;
+    cout << fat.size() << endl;
+    /*
+    cout << 1 << endl;
+    dat.push_front("7");
+    cout << 2 << endl;
+    dat.push_back("8");
+    cout << 3 << endl;
+    dat.push_front("8");
+    cout << 4 << endl;
+    cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << endl;
+    // prints: 8 7 9
+    cout << dat.size() << endl;  // prints 3 since there are 3 strings stored
+   
+    */
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -1,7 +1,10 @@
-foo
+
 #include <cstddef>
 #include <stdexcept>
 #include "ulliststr.h"
+#include <iostream>
+
+using namespace std;
 
 ULListStr::ULListStr()
 {
@@ -27,6 +30,127 @@ size_t ULListStr::size() const
 
 // WRITE YOUR CODE HERE
 
+void ULListStr::push_front(const std::string& val){
+
+  if(empty()){
+    head_ = tail_=new Item();
+    head_->last = ARRSIZE;
+    head_->first = ARRSIZE;
+  }
+  else if (head_->first == 0){
+    Item* temp = new Item();
+    temp->next = head_;
+    head_->prev = temp;
+    head_=temp;
+    head_->first = head_->last = ARRSIZE;
+  }
+
+  head_->val[head_->first -1] = val;
+  size_++;
+  head_->first --;
+
+}
+
+void ULListStr::push_back(const std::string& val){
+
+  if(empty()){
+    head_ = tail_ = new Item();
+  } 
+  else if (tail_->last == ARRSIZE){
+    Item* temp = new Item();
+    temp->prev = tail_;
+    tail_->next = temp;
+    tail_ = temp;
+  }
+  tail_->val[tail_->last] = val;
+  tail_->last++;
+  size_++;
+}
+
+void ULListStr::pop_front(){
+  head_->val[head_->first] = '\0';
+  head_->first++;
+  size_--;
+  if(head_->first == head_->last){
+    Item* temp = head_->next;
+    delete head_;
+    head_ = temp;
+  }
+}
+
+
+void ULListStr::pop_back(){
+  
+  if (tail_==nullptr){}
+  else if(tail_->last - tail_->first > 1){
+    tail_->last--;
+    size_--;
+  }
+  else{
+    if (head_ == tail_){
+      head_ = nullptr;
+      delete tail_;
+      tail_ = nullptr;
+    }
+    else{
+      Item* temp = tail_;
+      tail_ = tail_->prev;
+      delete temp;
+      tail_->next = nullptr;
+    }
+    size_ --;
+  }
+
+  /*
+  tail_->last--;
+  size_--;
+
+  if (size_==0){
+    delete tail_;
+  }
+  else if(tail_->last == 0){
+    Item* temp = tail_->prev;
+    temp->next = nullptr;
+    delete tail_;
+    tail_ = temp;
+  }
+  */
+}
+
+
+std::string const & ULListStr::back() const{
+  return (tail_->val[tail_->last - 1]);
+}
+
+std::string const & ULListStr::front() const{
+  return (head_->val[head_->first]);
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+
+  if (loc > size_){
+    return nullptr;
+  }
+  
+  int currfirst = head_->first;
+  Item* temp = head_;
+  string* spot;
+
+  for(size_t i = 0; i < loc + 1; i++){
+    if(currfirst == ARRSIZE){
+      currfirst = 0;
+      temp = temp->next;
+    }
+
+    spot = &(temp->val[currfirst]);
+    currfirst++;
+  }
+
+  return spot;
+}
+
+//End
+
 void ULListStr::set(size_t loc, const std::string& val)
 {
   std::string* ptr = getValAtLoc(loc);
@@ -38,7 +162,9 @@ void ULListStr::set(size_t loc, const std::string& val)
 
 std::string& ULListStr::get(size_t loc)
 {
+  //cout << "test" << endl;
   std::string* ptr = getValAtLoc(loc);
+  //cout << "test " << ptr << endl;
   if(ptr == NULL){
     throw std::invalid_argument("Bad location");
   }
